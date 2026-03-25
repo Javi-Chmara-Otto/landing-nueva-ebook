6cc922ffedbff96a0d2f62b59817f5a347dc9ff4
Javier Chmara Otto <javierchmaraotto@gmail.com>
2026-03-25 02:22:49 -0300

    update clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,6 +68,17 @@ export default function RootLayout({
         </Script>
         {/* --- FIN META PIXEL --- */}
 
+
+        {/* --- INICIO MICROSOFT CLARITY --- */}
+        <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vv6sel02tw");
+        </script>
+        {/* --- FIN MICROSOFT CLARITY --- */}
+
       </body>
     </html>
   )

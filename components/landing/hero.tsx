cae18b250d5f55076a9f40a0a43a6f83e313bcc1
Javier Chmara Otto <javierchmaraotto@gmail.com>
2026-04-17 13:37:48 -0300

    update mejor imagen

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -85,7 +85,7 @@ export function Hero() {
             <div className="relative group">
               <div className="relative w-72 sm:w-96 lg:w-[560px] transition-all duration-700 group-hover:scale-105">
                 <Image
-                  src="/Mockup_para_landing_2.webp"
+                  src="/mockup_2.webp"
                   alt="Ebook Velas"
                   width={500}
                   height={666}
